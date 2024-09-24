b51399ca99fb5970803e4543e17f48f0f57bbb95
mehedi-hasannnn <mhasan2999@gmail.com>
2024-09-24 22:16:13 +0600

    donation calculation functionalities added

---
+++ js/donation.js
@@ -0,0 +1,43 @@
+// Re usable function
+function addMoney(event, balanceId){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('add-money-input').value;
+
+    if( !isNaN(addMoneyInput) && addMoneyInput > 0 ){
+        const balance = document.getElementById(balanceId).innerText;
+        const accountBalance = document.getElementById('account-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const accountBalanceNumber = parseFloat(accountBalance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        const newAccountBalance = accountBalanceNumber - addMoneyNumber;
+        document.getElementById(balanceId).innerText = newBalance;
+        document.getElementById('account-balance').innerText = newAccountBalance;
+        document.getElementById('my_modal_5').showModal();
+    }
+
+    else{
+        alert('Failed to add your donation. Please insert a valid donation amount.');
+    }
+}
+
+// Noakhali part
+document.getElementById('btn-add-money').addEventListener('click', 
+function(event){
+    addMoney(event,'noa-balance') 
+})
+
+// Feni part
+document.getElementById('btn-add-money2').addEventListener('click', function(event){
+    addMoney(event,'feni-balance')
+})
+
+// Quota part
+document.getElementById('btn-add-money3').addEventListener('click', function(event){
+    addMoney(event, 'quota-balance')
+})
+
+
+document.getElementById('close-modal-btn').addEventListener('click', function() {
+    document.getElementById('my_modal_5').close();
+});
